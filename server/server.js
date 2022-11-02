9868680e17e7ab8a612c41d73c4922aaf4749d59
sergeykotenko <sergeykotenko@bk.ru>
2022-11-02 11:15:08 +0300

    deploy

--- server/server.js
+++ server/server.js
@@ -26,22 +26,20 @@ if (process.env.NODE_ENV === 'development') app.use(morgan('dev'))
 
 app.use(express.json())
 
-// const __dirname = path.resolve()
+const __dirname = path.resolve()
 
-// app.use('/uploads', express.static(path.join(__dirname, '/uploads/')))
+app.use('/uploads', express.static(path.join(__dirname, '/uploads/')))
 
-// app.use('/api/users', userRoutes)
-// app.use('/api/exercises', exerciseRoutes)
-// app.use('/api/workouts', workoutRoutes)
+app.use('/api/users', userRoutes)
+app.use('/api/exercises', exerciseRoutes)
+app.use('/api/workouts', workoutRoutes)
 
-// if (process.env.NODE_ENV === 'production') {
-// 	// Step 1:
-// 	app.use(express.static(path.resolve(__dirname, './client/build')))
-// 	// Step 2:
-// 	app.get('*', function (request, response) {
-// 		response.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
-// 	})
-// }
+// Step 1:
+app.use(express.static(path.resolve(__dirname, './client/build')))
+// Step 2:
+app.get('*', function (request, response) {
+	response.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
+})
 
 // app.use(notFound)
 // app.use(errorHandler)
